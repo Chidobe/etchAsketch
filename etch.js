e71edeeb6f2c0d9ec523a81b053d0c537f6b73e4
Chidobe <thedobecompany@gmail.com>
2022-11-21 22:43:36 +0100

    refactored my entire code, created functions to handle specific tasks, reused code instead of rewriting more lines

--- etch.js
+++ etch.js
@@ -1,28 +1,82 @@
-// Etch A Sketch 
 
-let gridValue = function () {
-    return prompt('How many squares per side do you want?')
-};
-
-const gridSizeBtn = document.querySelector('.grid-size-btn');
-gridSizeBtn.addEventListener('click', () => gridValue());
-
-
-
-let n = 50;
 
 const gridContainer = document.querySelector('.grid-container');
-gridContainer.setAttribute('style',`display: grid; grid-template-columns: repeat(${n}, 1fr)`)
+let containerSize = gridContainer.clientWidth
+const gridSizeBtn = document.querySelector('.grid-size-btn');
+const resetBtn = document.querySelector('.reset');
+//let squares = document.createElement('div');
 
 
+window.onload = createDefaultGridSize(16);
+createNewGrid();
+resetGridToDefault();
 
-let squares;
-
-// 400px is the width and height of the grid container
-
-for (let i = 0; i < (n ** 2); i++) {
-    squares = document.createElement('div');
-    squares.setAttribute('class', 'grid')
-    squares.setAttribute('style', `height:${400/n}px;width:${400/n}px`);
-    gridContainer.appendChild(squares)
+function createDefaultGridSize(n){
+    for (let i = 0; i < (n ** 2); i++) {
+                squares = document.createElement('div');
+                squares.setAttribute('class', 'grid');
+                //400px is the width and height of the grid container
+                squares.setAttribute('style', `height:${400 / n}px;width:${400 / n}px`);
+                gridContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${n}, 1fr)`);
+                gridContainer.appendChild(squares);
+    };
+    
 };
+
+
+function createNewGrid(){
+    gridSizeBtn.addEventListener('click', () => {
+        gridContainer.innerHTML = '';
+        createDefaultGridSize(prompt('how many squares per side?'))
+    })
+};
+
+
+function resetGridToDefault (){
+    resetBtn.addEventListener('click', () => {
+        gridContainer.innerHTML = '';
+        createDefaultGridSize(16);
+    });
+};
+
+
+
+
+// // Etch A Sketch 
+
+
+// //1. Get the nummber of sides the grids should have
+// let gridValue = function () {
+//     return prompt('How many squares per side do you want?')
+// };
+
+
+// //2. An 
+// const gridSizeBtn = document.querySelector('.grid-size-btn');
+// gridSizeBtn.addEventListener('click', () => { createGrid(gridValue()); });
+
+
+// //3. Creates the grid and sets its styling
+
+// function createGrid(n) {
+//     let squares;
+//     const gridContainer = document.querySelector('.grid-container');
+
+//     for (let i = 0; i < (n ** 2); i++) {
+//         squares = document.createElement('div');
+//         squares.setAttribute('class', 'grid');
+//         //400px is the width and height of the grid container
+//         squares.setAttribute('style', `height:${400 / n}px;width:${400 / n}px`);
+//         gridContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${n}, 1fr)`)
+//         gridContainer.appendChild(squares)    
+
+       
+//     };
+   
+// };
+
+
+
+   
+    
+    
