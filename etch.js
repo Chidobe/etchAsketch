80658a0052d4b79b940a9a3af02e6c061a7675b0
Chidobe <thedobecompany@gmail.com>
2022-11-27 15:44:42 +0100

    testing change of background on hover

--- etch.js
+++ etch.js
@@ -1,24 +1,28 @@
-
+//ETCH A SKETCH
 
 const gridContainer = document.querySelector('.grid-container');
 let containerSize = gridContainer.clientWidth
 const gridSizeBtn = document.querySelector('.grid-size-btn');
 const resetBtn = document.querySelector('.reset');
-//let squares = document.createElement('div');
+let squares;
 
 
 window.onload = createDefaultGridSize(16);
 createNewGrid();
 resetGridToDefault();
 
-function createDefaultGridSize(n){
-    for (let i = 0; i < (n ** 2); i++) {
+
+function createDefaultGridSize(numOfSq){
+    for (let i = 0; i < (numOfSq ** 2); i++) {
                 squares = document.createElement('div');
                 squares.setAttribute('class', 'grid');
-                //400px is the width and height of the grid container
-                squares.setAttribute('style', `height:${400 / n}px;width:${400 / n}px`);
-                gridContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${n}, 1fr)`);
+                squares.setAttribute('style', `height:${containerSize / numOfSq}px;width:${containerSize / numOfSq}px`);
+                gridContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${numOfSq}, 1fr)`);
                 gridContainer.appendChild(squares);
+
+                squares.addEventListener('mouseleave', () => {
+                    squares.setAttribute('style', `background-color: rgb(150, 10, 10)`)
+                })
     };
     
 };
@@ -38,42 +42,6 @@ function resetGridToDefault (){
         createDefaultGridSize(16);
     });
 };
-
-
-
-
-// // Etch A Sketch 
-
-
-// //1. Get the nummber of sides the grids should have
-// let gridValue = function () {
-//     return prompt('How many squares per side do you want?')
-// };
-
-
-// //2. An 
-// const gridSizeBtn = document.querySelector('.grid-size-btn');
-// gridSizeBtn.addEventListener('click', () => { createGrid(gridValue()); });
-
-
-// //3. Creates the grid and sets its styling
-
-// function createGrid(n) {
-//     let squares;
-//     const gridContainer = document.querySelector('.grid-container');
-
-//     for (let i = 0; i < (n ** 2); i++) {
-//         squares = document.createElement('div');
-//         squares.setAttribute('class', 'grid');
-//         //400px is the width and height of the grid container
-//         squares.setAttribute('style', `height:${400 / n}px;width:${400 / n}px`);
-//         gridContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${n}, 1fr)`)
-//         gridContainer.appendChild(squares)    
-
-       
-//     };
-   
-// };
 
 
 
