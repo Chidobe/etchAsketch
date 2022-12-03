92a6c45fe06e978568fcb0ba2956ced35032abff
Chidobe <thedobecompany@gmail.com>
2022-12-03 17:46:20 +0100

    Added random color selection option. I also added the option to change the number of squares in the grid using a slider range input

--- etch.js
+++ etch.js
@@ -2,16 +2,18 @@
 
 const gridContainer = document.querySelector('.grid-container');
 let containerSize = gridContainer.clientWidth
-const gridSizeBtn = document.querySelector('.grid-size-btn');
+const blackGridColor = document.querySelector('.black-grid-color');
 const resetBtn = document.querySelector('.reset');
 let squares;
+let slider = document.querySelector('.slider');
+sliderValueOutput = document.querySelector('.slider-value');
 
 
-window.onload = createDefaultGridSize(16);
+window.onload = createDefaultGridSize(slider.value);
 createNewGrid();
 resetGridToDefault();
 
-
+// Creates a 16 x 16 grid of squares; the default grid setting.
 function createDefaultGridSize(numOfSq) {
     for (let i = 0; i < (numOfSq ** 2); i++) {
         squares = document.createElement('div');
@@ -21,29 +23,47 @@ function createDefaultGridSize(numOfSq) {
         gridContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${numOfSq}, 1fr)`);
         gridContainer.appendChild(squares);
 
-
     };
-    document.querySelectorAll('.grid').forEach(square => {
-        square.addEventListener('mouseleave', () => {
-            square.setAttribute('style', `background-color: rgb(230, 110, 210)`)
+    // Adds an event listener to each square in the grid and sets its style value when the mouse enters the div
+    let allSquares = document.querySelectorAll('.grid');
+    allSquares.forEach(square => {
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = `rgb(${randomRgbValue()})`
         })
     });
 };
 
 
-
-
-function createNewGrid() {
-    gridSizeBtn.addEventListener('click', () => {
+// Resets the num of squares per side to default
+function resetGridToDefault() {
+    resetBtn.addEventListener('click', () => {
         gridContainer.innerHTML = '';
-        createDefaultGridSize(prompt('how many squares per side?'))
+        createDefaultGridSize(20);
+    });
+};
+
+// Generates random rgb() color values
+function randomRgbValue() {
+    let r = Math.round(Math.random() * 255);
+    let g = Math.round(Math.random() * 255);
+    let b = Math.round(Math.random() * 255);
+    return [r, g, b];
+};
+
+//Allows user to choose number of squares per side of the grid
+function createNewGrid() {
+    slider.addEventListener('input', () => {
+        gridContainer.innerHTML = '';
+        createDefaultGridSize(slider.value);
+        sliderValueOutput.textContent = `Grid: ${slider.value} X ${slider.value}`
     })
 };
 
 
-function resetGridToDefault() {
-    resetBtn.addEventListener('click', () => {
-        gridContainer.innerHTML = '';
-        createDefaultGridSize(16);
-    });
-};
+//Allows user to choose number of squares per side of the grid
+// function createNewGrid() {
+//     gridSizeBtn.addEventListener('click', () => {
+//         gridContainer.innerHTML = '';
+//         createDefaultGridSize(slider.value)
+//     })
+// }; find something else to do with this button!!!!!
